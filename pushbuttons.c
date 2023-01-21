cc6f3c7fef81c7d841c081d2439b61d5ebd62485
fatimask26 <123022049+fatimask26@users.noreply.github.com>
2023-01-21 11:04:22 +0200

    Create pushbuttons.c

---
+++ pushbuttons.c
@@ -0,0 +1,9 @@
+#include "Push_Buttons.h"
+
+void Push_Buttons_Init()
+{
+    MAKE_PIN_INPUT(Push_Buttons_Direction, Button_1);
+    MAKE_PIN_INPUT(Push_Buttons_Direction, Button_2);
+    MAKE_PIN_INPUT(Push_Buttons_Direction, Button_3);
+    MAKE_PIN_INPUT(Push_Buttons_Direction, Button_4);
+}
